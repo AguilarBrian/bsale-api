38e2577b71d64455148576f585a1caeb15270bb4
AguilarBrian <brianisaiasaguilar@gmail.com>
2022-10-13 21:22:43 -0300

    Change approach on connection port and route

--- app.js
+++ app.js
@@ -7,15 +7,22 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-// Set port depending on STATUS value (production or development).
-const port = process.env.STATUS === 'production' ? process.env.PROD_PORT : process.env.DEV_PORT;
 
-app.use(cors())
+// Set port depending on STATUS value (production or development).
+let port;
+
+if (process.env.STATUS === 'production') {
+    port = process.env.PROD_PORT || 4000;
+} else {
+    port = process.env.DEV_PORT || 3000;
+}
+
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use('/', routes);
+app.use('/api/v1/', routes);
 
-// Keep connection alive.
+// Ping DB connection every 5 seconds.
 setInterval(connectionPing, 5000)
 
 app.listen(port, console.log(`Server running on ${port}`));
